082d047169906105b82041933bdd4a62e96dabc4
AlfitoDinofa <alfitodinofa2006@gmail.com>
2026-03-13 15:36:24 +0700

    tambahkan fungsi display

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -48,5 +48,17 @@ void insertionsort()
         }
     }
 }
+void display(){
+    cout << endl;
+    cout << "\n================";
+    cout << "\nElement Array" << endl;
+    cout << "\n================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
 
 
